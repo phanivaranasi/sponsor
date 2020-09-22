17b448a0e5f85e59a9225dc427e09decbda3139f
Phaneendra V <varanasikrishnaphaneendra@gmail.com>
2020-09-22 21:49:43 +0530

    heroku build III

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-
-const publicPath = path.join(__dirname,'src','public');
+const publicPath = path.join(__dirname,'build');
 
 app.use(express.static(publicPath));
 
